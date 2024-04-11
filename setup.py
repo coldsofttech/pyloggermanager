9896f098a6f5e3537fc465cfab44e58b9a5254b7
coldsofttech <coldsoft.mail@gmail.com>
2024-04-11 19:16:27 +0100

    Updated Pipeline To Support Multi-OS

--- setup.py
+++ setup.py
@@ -15,5 +15,19 @@ setup(
     url='https://github.com/coldsofttech/pyloggermanager',
     license='MIT',
     author='coldsofttech',
-    description=pyloggermanager.__description__
+    description=pyloggermanager.__description__,
+    requires_python=">=3.10",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=["logger", "logging", "logging-framework", "logger-manager"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Logging",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
+    ]
 )
